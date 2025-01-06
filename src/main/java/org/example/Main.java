afec1760252f30163ab6612d4412967e5584d99c
nur-ilayda-kaplan <nurilaydakargi@gmail.com>
2025-01-06 19:22:46 +0300

    project is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,45 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+        return isBarking && (clock < 8 || clock >= 20);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if ((firstAge >= 13 && firstAge <= 19) || (secondAge >= 13 && secondAge <= 19) || (thirdAge >= 13 && thirdAge <= 19)) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if ((temp > 25 && temp <= 35) && !isSummer) {
+            return true;
+        } else if ((temp > 25 && temp < 45) && isSummer) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) {
+
+            return -1;
+        }
+            return width * height;
+
+
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        } else {
+            return radius * radius * Math.PI;
+        }
+
     }
 }
